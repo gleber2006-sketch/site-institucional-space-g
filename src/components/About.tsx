15432064c1275488b7298853b5b422188b87f1b5
devnicollaas <nicollasmencaccipereira@gmail.com>
2026-06-23 01:48:09 -0300

    altera label clientes atendidos para PROJETOS ENTREGUES

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@
 import RevealOnScroll from "./RevealOnScroll";
 
 const stats = [
-  { label: "clientes atendidos", value: "+200" },
+  { label: "PROJETOS ENTREGUES", value: "+200" },
   { label: "anos de experiência", value: "+6" },
   { label: "serviços especializados", value: "6" },
   { label: "Suporte próximo", value: "100%" },
